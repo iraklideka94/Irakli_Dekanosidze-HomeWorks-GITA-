0c7783e7214d5b085cbc35a23301c7251db6fd0e
iraklideka94 <iraklideka@gmail.com>
2024-11-15 18:01:24 +0400

    second task

--- app/src/main/java/com/example/gita_1/MainActivity.kt
+++ app/src/main/java/com/example/gita_1/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.example.gita_1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,11 +25,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.gita_1.ui.theme.CustomColor
+import androidx.compose.ui.unit.sp
 import com.example.gita_1.ui.theme.GITA1Theme
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GITA1Theme {
-LoginPage()
+                MainScreen()
             }
         }
     }
@@ -51,94 +45,105 @@ LoginPage()
 
 
 @Composable
-fun LoginPage() {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+fun MainScreen() {
+    var name by remember { mutableStateOf("") }
+    var location by remember { mutableStateOf("") }
+    var isChecked by remember { mutableStateOf(false) }
+    var displayText by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = CustomColor),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(250.dp)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(text = "Welcome Users", style = MaterialTheme.typography.headlineLarge,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 50.dp)
-        )
-
         Column(
             modifier = Modifier
-                .padding(30.dp)
+                .fillMaxSize()
+                .padding(top = 300.dp, start = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-
             TextField(
-                value = username,
-                onValueChange = { username = it },
-                label = { Text("Username") },
-                placeholder = {
-                    Text("Enter your username")
-                },
-                shape = RoundedCornerShape(10.dp),
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.Person,
-                        contentDescription = "UserName",
-                        tint = Color.Black
-                    )
-                },
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
-
             TextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                placeholder = {
-                    Text("Enter your password")
-                },
-                visualTransformation = PasswordVisualTransformation(),
-                shape = RoundedCornerShape(10.dp),
-                leadingIcon = {
-                    Icon(
-                        Icons.Rounded.Lock,
-                        contentDescription = null,
-                        tint = Color.Black
-                    )
-                },
+                value = location,
+                onValueChange = { location = it },
+                label = { Text("Location") },
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Checkbox(
+                    checked = isChecked,
+                    onCheckedChange = { isChecked = it }
+                )
+                Text(text = "Agree or Disagree")
+            }
 
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier.fillMaxWidth()
             ) {
+                Button(onClick = {
+                    if (isChecked) {
+                        Toast.makeText(
+                            context,
+                            "Name: $name, Location: $location",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Please Agree to the Terms and Conditions",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-
-                Button(onClick = {  }) {
-                    Text("Sign In")
+                }) {
+                    Text("Toast")
                 }
-                Button(onClick = {}) {
-                    Text("Sign Up")
+
+                Button(onClick = {
+                    if (isChecked) {
+                        displayText = "Name: $name, Location: $location"
+                    } else {
+                        displayText = "Please Agree to the Terms and  Conditions"
+                    }
+                }) {
+                    Text("Text")
                 }
             }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            if (displayText.isNotEmpty()) {
+                Text(
+                    text = displayText,
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 100.dp)
+                )
+            }
         }
-
-
     }
 }
-
-
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     GITA1Theme {
-       LoginPage()
+        MainScreen()
     }
 }
